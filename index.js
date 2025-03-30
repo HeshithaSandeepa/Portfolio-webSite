9a9011931677cde1b12ca00c1cc8e9090152d4e8
HeshithaSandeepa <hesithedg@gmail.com>
2025-03-30 14:07:23 +0530

    update my skill section

--- index.js
+++ index.js
@@ -111,3 +111,39 @@ const progressbarLines = document.querySelectorAll('.progress-line');
 progressbarLines.forEach((el) => {
     progressbar.observe(el);
 });
+
+
+
+//skills
+const skills = [
+    { name: "Html", img: "./images/html.png", level: "Expert" },
+    { name: "CSS", img: "./images/css.png", level: "Intermediate" },
+    { name: "Javascript", img: "./images/javascript.png", level: "Intermediate" },
+    { name: "Java", img: "./images/java.png", level: "Intermediate" },
+    { name: "SQL", img: "./images/sql.png", level: "Intermediate" },
+    { name: "Python", img: "./images/python.png", level: "Beginner" },
+    { name: "Bootstrap", img: "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png", level: "Intermediate" },
+    { name: "React", img: "https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg", level: "Beginner" },
+    { name: "Tailwind CSS", img: "https://tailwindcss.com/_next/static/media/tailwindcss-mark.d52e9897.svg", level: "beginner" },
+    { name: "Figma", img: "./images/figma.png", level: "Intermediate" },
+    { name: "PhotoShop", img: "https://logodownload.org/wp-content/uploads/2019/10/adobe-photoshop-logo.png", level: "Beginner" },
+
+
+];
+
+const skillsContainer = document.getElementById("skills-container");
+
+skills.forEach(skill => {
+    const skillCard = document.createElement("div");
+    skillCard.classList.add("skill-card");
+
+    skillCard.innerHTML = `
+        <img src="${skill.img}" alt="${skill.name}">
+        <p>${skill.name}</p>
+        <span class="skill-level">${skill.level}</span>
+    `;
+
+    skillsContainer.appendChild(skillCard);
+});
+
+
